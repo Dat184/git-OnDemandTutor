5566fad5beee7df456d14aa937c9d7ed21e1e5c4
chodu <shenlol184@gmail.com>
2024-07-24 22:00:38 +0700

    nhung buoc chan dau tien

---
+++ header.js
@@ -0,0 +1,14 @@
+function loadHeader() {
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        });
+}
+function loadFooter() {
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        });
+}
