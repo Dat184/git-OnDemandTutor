13cc34d2eff3d58ea781b8a74552d661a00b614a
Huy0123 <voquochuysn2004@gmail.com>
2024-07-24 21:59:01 +0700

    add file

---
+++ ondemandtutor/src/main/java/org/example/ondemandtutor/OndemandtutorApplication.java
@@ -0,0 +1,13 @@
+package org.example.ondemandtutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OndemandtutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OndemandtutorApplication.class, args);
+    }
+
+}
