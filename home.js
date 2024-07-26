214af0afc155b11b3e687acc7d535d94382d512a
Phan Van Tan <2251150034@ut.edu.vn>
2024-07-26 17:02:41 +0700

    trang home cua tan

---
+++ home.js
@@ -0,0 +1,29 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slide');
+    const totalSlides = slides.length;
+
+    if (index >= totalSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else {
+        currentSlide = index;
+    }
+
+    const slider = document.querySelector('.slider');
+    const offset = -currentSlide * 100;
+    slider.style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// Optional: Automatically advance slides every 5 seconds
+setInterval(nextSlide, 5000);
