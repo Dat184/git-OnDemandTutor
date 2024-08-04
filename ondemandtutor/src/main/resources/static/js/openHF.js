b19b0e55e54b4643413bf4f2c1cb634612435350
chodu <shenlol184@gmail.com>
2024-08-04 18:59:45 +0700

    update header

--- ondemandtutor/src/main/resources/static/js/openHF.js
+++ ondemandtutor/src/main/resources/static/js/openHF.js
@@ -3,12 +3,21 @@ function loadHeader() {
         .then(response => response.text())
         .then(data => {
             document.getElementById('header').innerHTML = data;
+            // Load the header.js script after the header is inserted
+            const script = document.createElement('script');
+            script.src = "js/header.js";
+            document.body.appendChild(script);
         });
+
 }
+
+
+
 function loadFooter() {
     fetch('footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer').innerHTML = data;
-        });
+        })
+        .catch(error => console.error('Error loading footer:', error));
 }
