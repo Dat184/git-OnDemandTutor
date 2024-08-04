3d47ef16f22b22a649d9ae6b41a823e331282f0e
chodu <shenlol184@gmail.com>
2024-08-04 16:50:55 +0700

    fix header

--- ondemandtutor/src/main/resources/static/js/header.js
+++ ondemandtutor/src/main/resources/static/js/header.js
@@ -1,14 +1,43 @@
-function loadHeader() {
-    fetch('header.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('header').innerHTML = data;
-        });
-}
-function loadFooter() {
-    fetch('footer.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('footer').innerHTML = data;
-        });
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const searchButton = document.getElementById('searchButton');
+    const searchInput = document.getElementById('searchInput');
+    const loginButton = document.getElementById('loginButton');
+    const avatar = document.getElementById('avatar');
+
+    // Xử lý khi nhấn vào nút tìm kiếm
+    searchButton.addEventListener('click', function(event) {
+        event.stopPropagation(); // Ngăn sự kiện click lan truyền lên document
+        searchButton.classList.toggle('active');
+        searchInput.classList.toggle('active');
+        searchInput.style.display = 'block'; // Ẩn nút đăng nhập
+        if (searchInput.classList.contains('active')) {
+            searchInput.focus(); // Đưa tiêu điểm vào thanh tìm kiếm khi nó được hiển thị
+        }
+        
+        else {
+            searchInput.style.display = 'block'; // Ẩn thanh tìm kiếm
+        }
+    });
+
+    // Xử lý khi nhấn vào bất kỳ đâu trên tài liệu
+    document.addEventListener('click', function(event) {
+        if (!searchButton.contains(event.target) && !searchInput.contains(event.target)) {
+            searchButton.classList.remove('active');
+            searchInput.classList.remove('active');
+            searchInput.style.border = 'block';
+            searchInput.style.display = 'block';
+        }
+    });
+
+    // Ngăn sự kiện click lan truyền từ thanh tìm kiếm
+    searchInput.addEventListener('click', function(event) {
+        event.stopPropagation(); // Ngăn sự kiện click lan truyền lên document
+       
+    });
+
+    // Xử lý khi nhấn vào nút đăng nhập
+    loginButton.addEventListener('click', function() {
+        loginButton.style.display = 'none'; // Ẩn nút đăng nhập
+        avatar.style.display = 'block'; // Hiển thị avatar
+    });
+});
